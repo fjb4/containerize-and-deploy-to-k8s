0922e120b3b162ce34f4b3d8fc725e036394e6ec
John Bush <fbush@vmware.com>
2021-09-24 16:03:49 -0500

    update k8s yaml

--- python-demo/app.py
+++ python-demo/app.py
@@ -1,6 +1,8 @@
+import os, datetime
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    pod_name = os.environ.get('POD_NAME')
+    return "<p>Hello, World from Python at {0}!</p><p>Pod Name: {1}</p>".format(datetime.datetime.now(), pod_name)
